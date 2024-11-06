6ca6be512503af85f44f22a0ff05ec9f8ad192d0
Mohammad <malam2003@berkeley.edu>
2024-11-05 19:32:55 -0800

    Initial Commit

---
+++ Shcoota/ShcootaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShcootaApp.swift
+//  Shcoota
+//
+//  Created by Mohammad Alam on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ShcootaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
